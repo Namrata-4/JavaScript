21178a3fb38084e9db208e69cd5f2c9b03a8140a
Namrata Bera <benamrata@gmail.com>
2026-03-17 23:30:31 +0530

    problem-solution

--- basic.js
+++ basic.js
@@ -12,93 +12,144 @@
 //  cant redeclear using const variable .
 
 //1. from string to array 
-const str="namrata";
-console.log(str);
-const arr=Array.from(str);
-console.log(arr);
-//2.loops(for loop)
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
-}
-//3.forEach loop
-arr.forEach((element)=>{
-    console.log(element);
-})
-//4.array methods 
-arr.push("bera");
-console.log(arr);
-const b=arr.pop();
-console.log(b);
-console.log(arr);
-//5.array sorting 
-const arr2=[1,5,3,6,3,7];
-console.log(arr2.sort());
-//6.indexof 
-const index=arr.indexOf("a");
-console.log(index);
-const index1=str.indexOf("x");
-console.log(index);
-//7.copy (two types - shallow copy-(in shallow copy there we use spread operator) and deep copy)
-const arra="nishan"
-arra[1]="N";
-console.log(arra);
-const coparr=[...arra];
-console.log(coparr);
-//8. rest operator 
-function sum(el1,el2){
-    return el1+el2;
-}
-console.log(sum(6,3));
-function sume(...elems){
-    let total=0;
-    for(x of elems){
-        total+=x;
-    }
-    return total;
-}
-console.log(sume(2,3,4,5));
-//9.cancationation of array 
-const aee=[1,2,3,4];
-const aee2=[5,6,7,8,9,10];
-const aee3=[...aee,...aee2];
-console.log(aee3);
-//10.string methods (touppar case, to lowercase,includes,length)
-const str2="nAmRata";
-// console.log(str2.toUpperCase);
-// console.log(str2.tolowerCase);
-//11.function 
-function greet(user){
-    console.log((`hello ${user}`));
-}
-greet("namrata");
-//12.find first and find last 
-const input=[3,6,5,4,3,7,-2,-5];
-const findfirst=input.find((num)=>num>5);
-const findlast=input.findLast((num)=>num>5);
-console.log(findfirst,findlast);
-//13.currying 
-function power(b){
-    return function(a){
-        return a**b;
-    }
-}
-const square=power(2);
-console.log(square(2));
-const cube=power(3);
-console.log(cube(2));
-//filter function,map function,reduce function 
-//1 filter 
-const positive=input.filter((num)=>num>0);
-console.log(positive);
-//2 map
-const numa=[2,5,6,1];
-const sqat=numa.map((num)=>{
-    console.log(num+num);
-});
-//3 reduce (accumulator that stores value , then variable , and then index )
-const sum2=input.reduce((total,num,index)=>{
-    console.log (total+num);
-},0);
-console.log(sum2);
+// const str="namrata";
+// console.log(str);
+// const arr=Array.from(str);
+// console.log(arr);
+// //2.loops(for loop)
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+// //3.forEach loop
+// arr.forEach((element)=>{
+//     console.log(element);
+// })
+// //4.array methods 
+// arr.push("bera");
+// console.log(arr);
+// const b=arr.pop();
+// console.log(b);
+// console.log(arr);
+// //5.array sorting 
+// const arr2=[1,5,3,6,3,7];
+// console.log(arr2.sort());
+// //6.indexof 
+// const index=arr.indexOf("a");
+// console.log(index);
+// const index1=str.indexOf("x");
+// console.log(index);
+// //7.copy (two types - shallow copy-(in shallow copy there we use spread operator) and deep copy)
+// const arra="nishan"
+// arra[1]="N";
+// console.log(arra);
+// const coparr=[...arra];
+// console.log(coparr);
+// //8. rest operator 
+// function sum(el1,el2){
+//     return el1+el2;
+// }
+// console.log(sum(6,3));
+// function sume(...elems){
+//     let total=0;
+//     for(x of elems){
+//         total+=x;
+//     }
+//     return total;
+// }
+// console.log(sume(2,3,4,5));
+// //9.cancationation of array 
+// const aee=[1,2,3,4];
+// const aee2=[5,6,7,8,9,10];
+// const aee3=[...aee,...aee2];
+// console.log(aee3);
+// //10.string methods (touppar case, to lowercase,includes,length)
+// const str2="nAmRata";
+// // console.log(str2.toUpperCase);
+// // console.log(str2.tolowerCase);
+// //11.function 
+// function greet(user){
+//     console.log((`hello ${user}`));
+// }
+// greet("namrata");
+// //12.find first and find last 
+// const input=[3,6,5,4,3,7,-2,-5];
+// const findfirst=input.find((num)=>num>5);
+// const findlast=input.findLast((num)=>num>5);
+// console.log(findfirst,findlast);
+// //13.currying 
+// function power(b){
+//     return function(a){
+//         return a**b;
+//     }
+// }
+// const square=power(2);
+// console.log(square(2));
+// const cube=power(3);
+// console.log(cube(2));
+// //filter function,map function,reduce function 
+// //1 filter 
+// const positive=input.filter((num)=>num>0);
+// console.log(positive);
+// //2 map
+// const numa=[2,5,6,1];
+// const sqat=numa.map((num)=>{
+//     console.log(num+num);
+// });
+// //3 reduce (accumulator that stores value , then variable , and then index )
+// const sum2=input.reduce((total,num,index)=>{
+//     console.log (total+num);
+// },0);
+// console.log(sum2);
 
+
+
+// const prompt = require('prompt-sync')();
+// let a=prompt("hey whats your age?");
+// a=Number.parseInt(a);
+// if(a>0){
+//     alert("this is a valid age");
+// }
+// else{
+//     alert("not valid");
+// }
+// const prompt = require('prompt-sync')();
+
+// let a = prompt("hey whats your age?");
+// console.log(a);
+// a=Number.parseInt(a);
+// console.log(a);
+// const notifier = require('node-notifier');
+// if(a>0){
+//     alert("this is a valid age");
+// }
+// else{
+//     alert("not valid");
+// };
+const prompt = require('prompt-sync')();
+const notifier = require('node-notifier'); // Import the notifier
+
+// STEP 1: Define 'alert' so Node.js knows what it is
+const alert = (message) => {
+    notifier.notify({
+        title: 'System Alert',
+        message: message,
+        sound: true, // Optional: plays a sound
+        wait: true   // Optional: waits for you to dismiss it
+    });
+    // Also log it to terminal so you see it in both places
+    console.log(`Alert: ${message}`); 
+};
+
+// --- Your Original Logic ---
+let a = prompt("hey whats your age? ");
+console.log("Input received:", a);
+
+a = Number.parseInt(a);
+console.log("Parsed number:", a);
+
+if (a > 0) {
+    alert("this is a valid age");
+} else {
+    alert("not valid");
+}
 
--- problemSolution.js
+++ problemSolution.js
@@ -1,23 +1,118 @@
 // problem number 1 --
-function lastdigit(num1,num2){
-    const rem1=num1%10;
-    const rem2=num2%10;
-    console.log(rem1==rem2 ? true : false);
-}
-lastdigit(12,24);
+// function lastdigit(num1,num2){
+//     const rem1=num1%10;
+//     const rem2=num2%10;
+//     console.log(rem1==rem2 ? true : false);
+// }
+// lastdigit(12,24);
 
-// problem number 2 --
-function commonDigits(num3,num4){
-    console.log(String(num3).length==String(num4).length? true : false);
-}
-commonDigits(78574,78666);
+// // problem number 2 --
+// function commonDigits(num3,num4){
+//     console.log(String(num3).length==String(num4).length? true : false);
+// }
+// commonDigits(78574,78666);
+// // problem number 5--
+// function transformNumbers (numbers){
+//     return numbers.map((num)=>{
+//         const square=num*num;
+//         return (num%2==0 ? square*2 : square);
+//     });
+// }
+// const arr=[2,4,6,8,7];
+// console.log(transformNumbers(arr));
 
-// problem number 3--
-function transformNumbers (numbers){
-    return numbers.map((num)=>{
-        const square=num*num;
-        return (num%2==0 ? square*2 : square);
-    });
-}
-const arr=[2,4,6,8,7];
-console.log(transformNumbers(arr));
+
+//problem number 3 , 4 , 6
+// function count(discount){
+//     function applyDiscount(price){
+//         return price-(price*discount);
+//     }
+// }
+// const discountPercentage=(10);
+// const productPrice=(100);
+
+
+// chap1-prob1
+// let name="namrata";
+// let num=1;
+// console.log(name+num);
+
+// //chap1-prob2
+// console.log(typeof(name+num));
+
+// //chap1-prob3
+// const a={
+//    name2:"nishan",
+//    age:20
+// }
+// // a=10;
+// a['lover']="namrata";
+// a['name2']="me";
+// console.log(a);
+
+// //chap1-prob4
+// const dict={
+//     Alacrity: "Ready eagerness",
+//     Benevolent: "Kind and well-meaning.",
+//     Candor: "Honesty and openness"
+// };
+// console.log(dict);
+// console.log(dict.Alacrity);
+
+//chap2-prob5
+// const prompt = require('prompt-sync')({sigint: true});
+// let age=prompt("whst is your age?");
+// if(age>10 && age<20){
+//     console.log("your age is between 10 and 20");
+// }
+// else{
+//     console.log("your age is  not between 10 and 20");
+// }
+
+//chap2-prob6
+// const prompt = require('prompt-sync')({sigint: true});
+// let age=prompt("whst is your age?");
+// switch(age){
+//     case '12':
+//      console.log("your age is 12")
+//      break
+//      case '13':
+//        console.log("your age is 13")
+//       break
+//      case '14':
+//         console.log("your age is 14")
+//         break
+//      case '15':
+//         console.log("your age is 15")
+//         break
+//     default:
+//         console.log("enter a valid age");
+// };
+
+//chap2-prob7
+// const prompt = require('prompt-sync')({sigint: true});
+// let number=prompt("enter a number : ");
+// number=Number.parseInt(number);
+// if(number%2==0 && number%3==0){
+//     console.log("the number is divisible by both 2 and 3");
+// }
+// else{
+//     console.log("the number is not divisible by both 2 and 3");
+// };
+
+//chap2-prob8
+// const prompt = require('prompt-sync')({sigint: true});
+// let number=prompt("enter a number : ");
+// number=Number.parseInt(number);
+// if(number%2==0 || number%3==0){
+//     console.log("the number is divisible ");
+// }
+// else{
+//     console.log("the number is not divisible");
+// };
+
+//chap2-prob8
+ const prompt = require('prompt-sync')({sigint: true});
+let age=prompt("whst is your age?");
+const age1=Number.parseInt(age);
+console.log(age1>18 ? "you can drive" : "you cant drive");
